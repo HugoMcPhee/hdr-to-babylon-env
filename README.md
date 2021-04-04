fc6e9cf9ac6784179f7a104927c2dc9aa097550d
Hugo McPhee <mcphee.hugo@gmail.com>
2021-04-04 14:00:41 +1000

    adds optional resolution argument
    - and changes back to non headless mode for speed

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ An npx tool to convert hdr files to env files (requires chrome)
 run this command in a folder with hdr files to make .env copies  
 `npx github:HugoMcPhee/hdr-to-babylon-env`
 
+by default it uses a resolution of 256  
+but can be changed with the first argument  
+`npx github:HugoMcPhee/hdr-to-babylon-env 128`
+
 references  
 guide for using puppeteer-core  
 https://dev.to/sonyarianto/practical-puppeteer-playing-with-puppeteer-core-7i0
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ var path = require("path");
   // const nodeScriptPath = __dirname;
   const folderPath = process.cwd();
 
+  let probeResolution = process.argv[2] || 256;
+  probeResolution = parseInt(probeResolution, 10);
+
   // type HDRFileProbeData = { name: "probe.hdr" , data: base64String}
   const hdrFilesData = []; // as HDRFileProbeData[]
 
@@ -33,100 +36,107 @@ var path = require("path");
   await Promise.all(files.map((fileName) => checkDirectoryItem(fileName)));
 
   let launchOptions = {
-    headless: true,
+    headless: false,
     executablePath: chromePaths.chrome,
+    args: [`--window-size=100,100`],
   };
 
   const browser = await puppeteer.launch(launchOptions);
   const page = await browser.newPage();
   await page.addScriptTag({ url: "https://cdn.babylonjs.com/babylon.js" });
 
+  console.log("found");
   console.log(hdrFilesData.map((item) => item.name));
+  console.log(`converting to env files with a size of ${probeResolution}`);
 
-  const envFilesData = await page.evaluate(async (hdrFilesData) => {
-    // type EnvFileData = { name: "probe.hdr" , data: binaryString}
-    const envFilesData = []; // as EnvFileData[]
+  const envFilesData = await page.evaluate(
+    async (hdrFilesData, probeResolution) => {
+      // type EnvFileData = { name: "probe.hdr" , data: binaryString}
+      const envFilesData = []; // as EnvFileData[]
 
-    // ----------------------------------
-    // Setting up a babylonjs scene
-    // ----------------------------------
-    var canvas = document.createElement("canvas");
-    canvas.id = "renderCanvas";
-    document.body.appendChild(canvas);
+      // ----------------------------------
+      // Setting up a babylonjs scene
+      // ----------------------------------
+      var canvas = document.createElement("canvas");
+      canvas.id = "renderCanvas";
+      document.body.appendChild(canvas);
 
-    var engine = new BABYLON.Engine(canvas, true, {
-      preserveDrawingBuffer: true,
-      stencil: true,
-      premultipliedAlpha: false,
-    });
-    var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.FreeCamera(
-      "camera1",
-      new BABYLON.Vector3(0, 5, -10),
-      scene
-    );
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, true);
+      var engine = new BABYLON.Engine(canvas, true, {
+        preserveDrawingBuffer: true,
+        stencil: true,
+        premultipliedAlpha: false,
+      });
+      var scene = new BABYLON.Scene(engine);
+      var camera = new BABYLON.FreeCamera(
+        "camera1",
+        new BABYLON.Vector3(0, 5, -10),
+        scene
+      );
+      camera.setTarget(BABYLON.Vector3.Zero());
+      camera.attachControl(canvas, true);
 
-    // ----------------------------------
-    // Converting HDR to Env
-    // ----------------------------------
+      // ----------------------------------
+      // Converting HDR to Env
+      // ----------------------------------
 
-    async function waitForSceneReady() {
-      return new Promise(async (resolve, reject) => {
-        scene.executeWhenReady(() => {
-          resolve();
+      async function waitForSceneReady() {
+        return new Promise(async (resolve, reject) => {
+          scene.executeWhenReady(() => {
+            resolve();
+          });
         });
-      });
-    }
-    async function getBlobAsBinaryString(theBlob) {
-      return new Promise(async (resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsBinaryString(theBlob);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = () =>
-          reject("Error occurred while reading binary string");
-      });
-    }
-    async function getEnvFileBinaryStringFromHdrString(hdrString) {
-      const environment = new BABYLON.HDRCubeTexture(
-        hdrString,
-        scene,
-        128,
-        false,
-        true,
-        false,
-        true
-      );
+      }
+      async function getBlobAsBinaryString(theBlob) {
+        return new Promise(async (resolve, reject) => {
+          const reader = new FileReader();
+          reader.readAsBinaryString(theBlob);
+          reader.onload = () => resolve(reader.result);
+          reader.onerror = () =>
+            reject("Error occurred while reading binary string");
+        });
+      }
+      async function getEnvFileBinaryStringFromHdrString(hdrString) {
+        const environment = new BABYLON.HDRCubeTexture(
+          hdrString,
+          scene,
+          parseInt(probeResolution, 10),
+          false,
+          true,
+          false,
+          true
+        );
+        await waitForSceneReady();
+        const arrayBuffer = await BABYLON.EnvironmentTextureTools.CreateEnvTextureAsync(
+          environment
+        );
+        var blob = new Blob([arrayBuffer], { type: "octet/stream" });
+        const binaryFileResult = await getBlobAsBinaryString(blob);
+        environment.dispose();
+        return binaryFileResult;
+      }
+
+      async function getEnvFileDataFromHdrFileData(hdrDataItem) {
+        const newData = await getEnvFileBinaryStringFromHdrString(
+          hdrDataItem.data
+        );
+        return {
+          data: newData,
+          name: hdrDataItem.name.replace(".hdr", ".env"),
+        };
+      }
+
       await waitForSceneReady();
-      const arrayBuffer = await BABYLON.EnvironmentTextureTools.CreateEnvTextureAsync(
-        environment
-      );
-      var blob = new Blob([arrayBuffer], { type: "octet/stream" });
-      const binaryFileResult = await getBlobAsBinaryString(blob);
-      environment.dispose();
-      return binaryFileResult;
-    }
 
-    async function getEnvFileDataFromHdrFileData(hdrDataItem) {
-      const newData = await getEnvFileBinaryStringFromHdrString(
-        hdrDataItem.data
-      );
-      return {
-        data: newData,
-        name: hdrDataItem.name.replace(".hdr", ".env"),
-      };
-    }
+      for (const hdrFileData of hdrFilesData) {
+        const envFileData = await getEnvFileDataFromHdrFileData(hdrFileData);
+        envFilesData.push(envFileData);
+      }
 
-    await waitForSceneReady();
-
-    for (const hdrFileData of hdrFilesData) {
-      const envFileData = await getEnvFileDataFromHdrFileData(hdrFileData);
-      envFilesData.push(envFileData);
-    }
-
-    return envFilesData;
-  }, hdrFilesData);
+      return envFilesData;
+    },
+    hdrFilesData,
+    probeResolution
+  );
 
   async function writeEnvFileDataToFile(envDataItem) {
     const envFilePath = path.join(folderPath, envDataItem.name);
